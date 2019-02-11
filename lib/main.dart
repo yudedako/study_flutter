e5903e5f8c471e4b23ca33e1eeb14aa00d8315cf
yudedako <pluto.club@gmail.com>
2019-02-11 14:59:37 +0900

    データベースアクセスの実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:collection/collection.dart';
+import 'package:path/path.dart';
 
 void main() {
   runApp(new MyApp());
@@ -16,11 +20,10 @@ class MyApp extends StatelessWidget {
         canvasColor: const Color(0xFFfafafa),
       ),
       home: new FirstScreen(),
-      initialRoute: '/first',
+      initialRoute: '/home',
       routes: {
-        '/first': (context) => FirstScreen(),
-        '/second': (context) => SecondScreen('Second'),
-        '/third': (context) => SecondScreen('Third'),
+        '/home': (context) => FirstScreen(),
+        '/list': (context) => SecondScreen(),
       },
     );
   }
@@ -34,8 +37,18 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
-  //final _controller = TextEditingController();
-  //String _input;
+  final _controllerA = TextEditingController();
+  final _controllerB = TextEditingController();
+  final _controllerC = TextEditingController();
+
+  final TextStyle styleA = TextStyle(
+    fontSize: 28.0,
+    color: Colors.black87,
+  );
+  final TextStyle styleB = TextStyle(
+    fontSize: 24.0,
+    color: Colors.black87,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -44,79 +57,128 @@ class _FirstScreenState extends State<FirstScreen> {
         title: Text('Home'),
       ),
 
-      body: Center(
-        child: Text('Home Screen',
-          style: TextStyle(fontSize: 32.0),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Home Screen',
+              style: TextStyle(fontSize: 32.0),
+            ),
+
+            Text( 'Name:', style: styleB, ),
+            TextField(
+              controller: _controllerA,
+              style: styleA,
+            ),
+
+            Text( 'Mail:', style: styleB, ),
+            TextField(
+              controller: _controllerB,
+              style: styleA,
+            ),
+
+            Text( 'Tel:', style: styleB, ),
+            TextField(
+              controller: _controllerC,
+              style: styleA,
+            ),
+          ],
         ),
       ),
-
-      // body: Column(
-      //   children: <Widget>[
-      //     Text('Home Screen',
-      //       style: TextStyle(fontSize: 32.0)
-      //     ),
-      //     Padding(
-      //       padding: EdgeInsets.all(10.0),
-      //     ),
-      //     TextField(
-      //       controller: _controller,
-      //       style: TextStyle(
-      //         fontSize: 28.0,
-      //         color: const Color(0xFFFF0000),
-      //         fontWeight: FontWeight.w400,
-      //         fontFamily: "Roboto"
-      //       ),
-      //       onChanged: changeField,
-      //     ),
-      //   ],
-      // ),
 
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
 
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            title: Text('Home'),
+            title: Text('add'),
             icon: Icon(Icons.home),
           ),
           BottomNavigationBarItem(
-            title: Text('next'),
-            icon: Icon(Icons.navigate_next),
+            title: Text('list'),
+            icon: Icon(Icons.list),
           ),
         ],
 
         onTap: (int value) {
-          if (value == 1)
-            Navigator.pushNamed(
-              context,
-              '/second',
-              //MaterialPageRoute(builder: (context) => SecondScreen(_input)),
-            );
+          if (value == 1) {
+            Navigator.pushNamed(context, '/list');
+          }
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {
+          saveData();
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text("saved!"),
+              content: Text("insert data  into database."),
+            )
+          );
         },
       ),
     );
   }
 
-  //void changeField(String val) => _input = val;
+  void saveData() async {
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, "mydata.db");
+
+    String data1 = _controllerA.text;
+    String data2 = _controllerB.text;
+    String data3 = _controllerC.text;
+
+    String query = 'INSERT INTO mydata(name, mail, tel) VALUES("$data1", "$data2", "$data3")';
+
+    Database database = await openDatabase(
+      path, version: 1, onCreate: (Database db, int version) async {
+        await db.execute(
+          "CREATE TABLE IF NOT EXISTS mydata (id INTEGER PRIMARY KEY, name TEXT, mail TEXT, tel TEXT)"
+        );
+      }
+    );
+
+    await database.transaction((txn) async {
+      int id = await txn.rawInsert(query);
+      print("insert: $id");
+    });
+
+    setState(() {
+      _controllerA.text = '';
+      _controllerB.text = '';
+      _controllerC.text = '';
+    });
+  }
 }
 
-class SecondScreen extends StatelessWidget {
-  final String _value;
+class SecondScreen extends StatefulWidget {
+  SecondScreen({ Key key }) : super(key: key);
 
-  SecondScreen(this._value);
+  @override
+  _SecondScreenState createState() => new _SecondScreenState();
+}
+
+class _SecondScreenState extends State <SecondScreen> {
+  List<Widget> _items = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    getItems();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Next"),
+        title: Text("List"),
       ),
 
-      body: Center(
-        child: Text(
-          '$_value Screen',
-          style: TextStyle(fontSize: 32.0)
-        ),
+      body: ListView(
+        children: _items,
       ),
 
       bottomNavigationBar: BottomNavigationBar(
@@ -124,24 +186,50 @@ class SecondScreen extends StatelessWidget {
 
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            title: Text('prev'),
-            icon: Icon(Icons.navigate_before),
+            title: Text('add'),
+            icon: Icon(Icons.home),
           ),
           BottomNavigationBarItem(
-            title: Text('?'),
-            icon: Icon(Icons.android),
+            title: Text('list'),
+            icon: Icon(Icons.list),
           ),
         ],
 
-        onTap: (int value) {
-          if (value == 0) Navigator.pop(context);
-          if (value == 1)
-            Navigator.pushNamed(
-              context,
-              '/third',
-            );
+        onTap: (int index) {
+          if (index == 0) {
+            Navigator.pop(context);
+          }
         },
       ),
     );
+  }
+
+  void getItems() async {
+    List<Widget> list = <Widget>[];
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, "mydata.db");
+
+    Database database = await openDatabase(
+      path, version: 1, onCreate: (Database db, int version) async {
+        await db.execute(
+          "CREATE TABLE IF NOT EXISTS mydata (id INTEGER PRIMARY KEY, name TEXT, mail TEXT, tel TEXT)"
+        );
+      }
+    );
+
+    List<Map> result = await database.rawQuery('SELECT * FROM mydata');
+
+    for (Map item in result) {
+      list.add(
+        ListTile(
+          title: Text(item['name']),
+          subtitle: Text(item['mail'] + ' ' + item['tel']),
+        )
+      );
+    }
+
+    setState(() {
+      _items = list;
+    });
   }
 }
