69b4049ae160f859fd97e15ce83990860a069224
yudedako <pluto.club@gmail.com>
2019-02-17 11:28:31 +0900

    設定の修正

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,47 @@ class MyApp extends StatelessWidget {
 }
 
 class List extends StatefulWidget {
+  @override
+  _MyList createState() => _MyList();
+}
+
+class _MyList extends State<List> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("リスト画面"),
       ),
+
+      body:Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream:Firestore.instance.collection('study-kasikari-memo').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              padding: const EdgeInsets.only(top: 10.0),
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index]),
+            );
+          }
+        ),
+      ),
     );
   }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.android),
+            title: Text("【" + (document['borrowOrLend'] == "lend" ? "貸" : "借") + "】" + document['stuff']),
+            subtitle: Text('期限： ' + document['date'].toString().substring(0, 10) + "\n相手： " + document['user']),
+          ),
+        ]
+      ),
+    );
   }
 }
